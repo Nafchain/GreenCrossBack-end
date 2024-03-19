12dc4fc8276842b81583f889974d149ad493c871
Freddiew-117 <fredyjerbi@gmail.com>
2024-03-19 18:34:39 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const express =require('express');
-const conectDB= require('./Config/dataBase');
-const cors =require("cors");
-const app=express();
+const express = require('express');
+const conectDB = require('./Config/dataBase');
+const cors = require("cors");
+const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => { res.send("Express on Vercel"); }); 
 
+app.listen(PORT, () => { 
+    console.log(`Server is running on port ${PORT}`); 
+});
 
+module.exports = app;
 
 conectDB();
 app.use(cors());
@@ -14,6 +19,6 @@ app.use(express.json());
 app.use('/api/user', require('./Routes/routes'));
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log('The server is runing');
 })
